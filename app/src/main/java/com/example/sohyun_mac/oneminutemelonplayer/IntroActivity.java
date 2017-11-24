4b9086e0b66de8ed011a43d0b099d8df1867b143
SohyunDev <mohazzang@gmail.com>
2017-11-24 18:47:11 +0900

    add Intro activity and background image of intro

--- app/src/main/java/com/example/sohyun_mac/oneminutemelonplayer/IntroActivity.java
+++ app/src/main/java/com/example/sohyun_mac/oneminutemelonplayer/IntroActivity.java
@@ -1,13 +1,38 @@
 package com.example.sohyun_mac.oneminutemelonplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 public class IntroActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro);
+        handler = new Handler();
+        handler.postDelayed(run, 3000);
     }
+
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            Intent mainIntent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        }
+    };
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        handler.removeCallbacks(run);
+    }
+
 }
